60433ad26076d80386ec006763ab609fa10fc96f
korzh260609-beep <korzh260609@gmail.com>
2025-12-04 08:37:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import * as Sources from "./sources.js"; // скелет слоя источни
 import { classifyInteraction } from "./classifier.js"; // скелет классификатора
 import { callAI } from "./ai.js"; // универсальный вызов ИИ
 import { buildSystemPrompt } from "./systemPrompt.js";
+import { getProjectSection, upsertProjectSection } from "./projectMemory.js";
 
 // === Константы ===
 const MAX_HISTORY_MESSAGES = 20;

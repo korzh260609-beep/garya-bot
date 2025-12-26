622d88dd76360430138e2b07b2d144ceea031997
korzh260609-beep <korzh260609@gmail.com>
2025-12-26 08:39:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // ============================================================================
 // === index.js — SG (Советник GARYA) : Express + Telegram Webhook + Commands ===
 // ============================================================================
-console.log("BOOT ROOT INDEX OK");
 
 import express from "express";
 import TelegramBot from "node-telegram-bot-api";

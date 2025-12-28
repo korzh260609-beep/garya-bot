53b6ba1a5c11e3588589940e47eb87251c616372
korzh260609-beep <korzh260609@gmail.com>
2025-12-28 09:10:13 +0200

    Update server.js

--- src/http/server.js
+++ src/http/server.js
@@ -1,1 +1,14 @@
+import express from "express";
+
+export function createApp() {
+  const app = express();
+  app.use(express.json());
+  return app;
+}
+
+export function startHttpServer(app, port) {
+  app.listen(port, async () => {
+    console.log("🌐 HTTP-сервер запущен на порту:", port);
+  });
+}
 

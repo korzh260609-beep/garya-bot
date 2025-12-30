e9dfbf4d3404a7e35f8224d12eba802e68bc3227
korzh260609-beep <korzh260609@gmail.com>
2025-12-30 22:20:07 +0200

    Update messageHandler.js

--- src/bot/messageHandler.js
+++ src/bot/messageHandler.js
@@ -1,1 +1,10 @@
+// src/bot/messageHandler.js
+// ВНИМАНИЕ: сюда переносим bot.on("message", ...) целиком без изменений логики.
+
+export function initMessageHandler({ bot }) {
+  bot.on("message", async (msg) => {
+    // TODO: ВСТАВЬ СЮДА ЦЕЛИКОМ ВЕСЬ ТВОЙ ТЕКУЩИЙ БЛОК:
+    // bot.on("message", async (msg) => { ... });
+  });
+}
 

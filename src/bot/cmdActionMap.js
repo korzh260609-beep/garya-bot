40e2b513b1a9ab5d240c9653218fc09e1b180564
korzh260609-beep <korzh260609@gmail.com>
2026-02-08 08:57:40 +0200

    Update cmdActionMap.js

--- src/bot/cmdActionMap.js
+++ src/bot/cmdActionMap.js
@@ -7,8 +7,6 @@ export const CMD_ACTION = {
 
   "/mode": "cmd.mode",
 
-  "/reindex": "cmd.repo.reindex",
-
   "/tasks": "cmd.tasks.list",
   "/run": "cmd.task.run",
   "/newtask": "cmd.task.create",

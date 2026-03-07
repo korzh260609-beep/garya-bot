87d68cf12043763243ec1a1862b0844616cee445
korzh260609-beep <korzh260609@gmail.com>
2026-03-07 08:36:05 +0200

    Create decision/decisionTypes.js

---
+++ src/decision/decisionTypes.js
@@ -0,0 +1,9 @@
+export const DECISION_KIND = {
+  CHAT_SIMPLE: "chat_simple",
+  CHAT_COMPLEX: "chat_complex",
+  TASK_EXECUTION: "task_execution",
+  SOURCE_QUERY: "source_query",
+  REPO_ANALYSIS: "repo_analysis",
+  SYSTEM_DIAG: "system_diag",
+  UNKNOWN: "unknown",
+};

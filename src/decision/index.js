e184c8ef36c274b20a9d8e725641a2edba131a55
korzh260609-beep <korzh260609@gmail.com>
2026-03-07 09:08:23 +0200

    Update index.js

--- src/decision/index.js
+++ src/decision/index.js
@@ -2,9 +2,7 @@ export { DECISION_KIND } from "./decisionTypes.js";
 
 export { createDecisionContext } from "./decisionContext.js";
 
-export { createDecisionRoute } from "./decisionRoute.js";
-
-export { routeDecision } from "./decisionRouter.js";
+export { createDecisionRoute, routeDecision } from "./decisionRouter.js";
 
 export { runDecisionWorker } from "./decisionWorker.js";
 

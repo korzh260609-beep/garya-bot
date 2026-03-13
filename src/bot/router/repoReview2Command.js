60fb5700384f7bb6566e9d3d2d0358db3dca2c1a
korzh260609-beep <korzh260609@gmail.com>
2026-03-13 22:38:35 +0200

    Create repoReview2Command.js

---
+++ src/bot/router/repoReview2Command.js
@@ -0,0 +1,12 @@
+// src/bot/router/repoReview2Command.js
+
+export async function handleRepoReview2Command({
+  handleRepoReview2,
+  bot,
+  chatId,
+}) {
+  await handleRepoReview2({
+    bot,
+    chatId,
+  });
+}

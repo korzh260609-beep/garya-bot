d258c73fcf20450663aaa8c4c485a3d3ad8161c3
korzh260609-beep <korzh260609@gmail.com>
2026-03-23 16:33:19 +0200

    Create buildDefaultChatLongTermPromptSelector.js

---
+++ src/core/buildDefaultChatLongTermPromptSelector.js
@@ -0,0 +1,28 @@
+// src/core/buildDefaultChatLongTermSelector.js
+//
+// Goal:
+// - centralize default LTM selector for chat flow
+// - keep bridge clean
+// - no DB
+// - no side effects
+// - deterministic only
+
+import buildLongTermPromptSelector from "./buildLongTermPromptSelector.js";
+
+export function buildDefaultChatLongTermSelector() {
+  return buildLongTermPromptSelector({
+    rememberTypes: [
+      "user_profile",
+      "vehicle_profile",
+      "maintenance_fact",
+      "maintenance_interval",
+      "task_intent",
+    ],
+    rememberKeys: ["communication_style"],
+    perTypeLimit: 3,
+    perKeyLimit: 3,
+    totalLimit: 12,
+  });
+}
+
+export default buildDefaultChatLongTermSelector;

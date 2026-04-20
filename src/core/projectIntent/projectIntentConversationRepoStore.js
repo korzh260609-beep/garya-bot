c363682d1e0bbafa5333c110fd36b644e347a38a
korzh260609-beep <korzh260609@gmail.com>
2026-04-20 08:52:16 +0300

    Update projectIntentConversationRepoStore.js

--- src/core/projectIntent/projectIntentConversationRepoStore.js
+++ src/core/projectIntent/projectIntentConversationRepoStore.js
@@ -35,3 +35,63 @@ export {
   pickBestSearchMatch,
   pickLikelyTargetPath,
 } from "./repoStore/projectIntentRepoStoreTargetPicker.js";
+
+import {
+  basenameOf,
+  hasPathSeparator,
+  isFileLike,
+  isFolderLike,
+  normalizeFolderPath,
+  isBareBasenameLike,
+  resolveCanonicalPathFromBasename,
+} from "./repoStore/projectIntentRepoStorePathUtils.js";
+
+import {
+  loadLatestSnapshot,
+  pathExistsInSnapshot,
+  pathKindInSnapshot,
+  fetchPathsByPrefix,
+  fetchAllSnapshotPaths,
+  computeImmediateChildren,
+} from "./repoStore/projectIntentRepoStoreSnapshot.js";
+
+import {
+  scoreBasenameMatch,
+  sortByScoreThenPath,
+  rankPathCandidate,
+  searchSnapshotPaths,
+} from "./repoStore/projectIntentRepoStoreSearch.js";
+
+import {
+  fetchRepoFileText,
+} from "./repoStore/projectIntentRepoStoreFileReader.js";
+
+import {
+  pickLikelyTargetPathFromKnownEntity,
+  pickBestSearchMatch,
+  pickLikelyTargetPath,
+} from "./repoStore/projectIntentRepoStoreTargetPicker.js";
+
+export default {
+  basenameOf,
+  hasPathSeparator,
+  isFileLike,
+  isFolderLike,
+  normalizeFolderPath,
+  isBareBasenameLike,
+  resolveCanonicalPathFromBasename,
+  loadLatestSnapshot,
+  pathExistsInSnapshot,
+  pathKindInSnapshot,
+  fetchPathsByPrefix,
+  fetchAllSnapshotPaths,
+  computeImmediateChildren,
+  scoreBasenameMatch,
+  sortByScoreThenPath,
+  rankPathCandidate,
+  searchSnapshotPaths,
+  fetchRepoFileText,
+  pickLikelyTargetPathFromKnownEntity,
+  pickBestSearchMatch,
+  pickLikelyTargetPath,
+};

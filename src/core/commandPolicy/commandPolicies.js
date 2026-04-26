d963764829e4c2bd9b712b4942ef113e8bbd7a8a
korzh260609-beep <korzh260609@gmail.com>
2026-04-26 10:56:23 +0300

    Add command policy shadow last to policy config

--- src/core/commandPolicy/commandPolicies.js
+++ src/core/commandPolicy/commandPolicies.js
@@ -95,6 +95,7 @@ export const MEMORY_DIAGNOSTIC_COMMAND_POLICIES = Object.freeze([
   privateCommand("/diag_decision_promotion", COMMAND_POLICY_SCOPES.MEMORY_DIAGNOSTICS),
   privateCommand("/command_policy_diag", COMMAND_POLICY_SCOPES.MEMORY_DIAGNOSTICS),
   privateCommand("/command_policy_selftest", COMMAND_POLICY_SCOPES.MEMORY_DIAGNOSTICS),
+  privateCommand("/command_policy_shadow_last", COMMAND_POLICY_SCOPES.MEMORY_DIAGNOSTICS),
 ]);
 
 export const SOURCE_COMMAND_POLICIES = Object.freeze([

a05ffe0b027ea2f65b400d308fc131f59bc23c9f
korzh260609-beep <korzh260609@gmail.com>
2026-05-06 11:14:59 +0300

    refactor: add SG prompt module barrel

---
+++ src/core/prompt/index.js
@@ -0,0 +1,12 @@
+// AGENT NOTE:
+// SG 2.0 prompt module barrel.
+// Purpose: provide stable imports for system prompt sections.
+
+export * from "./promptIdentity.js";
+export * from "./promptBehaviorRules.js";
+export * from "./promptLanguageRules.js";
+export * from "./promptRuntimeContext.js";
+export * from "./promptGithubRuntime.js";
+export * from "./promptGithubWritePolicy.js";
+export * from "./promptGithubOutputFormat.js";
+export * from "./promptRuntimeStatus.js";

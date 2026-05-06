4b37cf25b31c9ed71bb8a1c9d64192475d6d10e0
korzh260609-beep <korzh260609@gmail.com>
2026-05-06 16:26:44 +0300

    Attach Agent Workspace HTTP routes

--- src/app/appFactory.js
+++ src/app/appFactory.js
@@ -7,16 +7,23 @@ import express from "express";
 import { initTelegramTransport } from "../transport/telegram.js";
 import { attachRootRoutes } from "./rootRoutes.js";
 import { attachHealthRoutes } from "./healthRoutes.js";
+import { attachAgentWorkspaceRoutes } from "./agentWorkspaceRoutes.js";
 
 export function createApp() {
   const app = express();
 
-  app.use(express.json({ limit: "256kb" }));
+  app.use(express.json({
+    limit: "256kb",
+    verify: (req, res, buf) => {
+      req.rawBody = buf;
+    },
+  }));
 
   const telegramBot = initTelegramTransport(app);
 
   attachRootRoutes(app);
   attachHealthRoutes(app, { telegramBot });
+  attachAgentWorkspaceRoutes(app);
 
   return {
     app,

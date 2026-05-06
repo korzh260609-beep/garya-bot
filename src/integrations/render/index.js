288b1c9b6abc6a5f84a0ee021ccee6114ba9b347
korzh260609-beep <korzh260609@gmail.com>
2026-05-06 13:43:11 +0300

    feat: add Render integration exports

---
+++ src/integrations/render/index.js
@@ -0,0 +1,7 @@
+// AGENT NOTE:
+// SG 2.0 Render integration boundary.
+// Export skeleton contracts only; do not connect runtime here.
+
+export * from "./RenderConfig.js";
+export * from "./RenderNormalizer.js";
+export * from "./RenderClient.js";

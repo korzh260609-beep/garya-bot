250fdbff3f5bc94fe4f0867dc4e1b8c5ed6eec7c
korzh260609-beep <korzh260609@gmail.com>
2026-05-06 11:36:52 +0300

    refactor: add core message module barrel

---
+++ src/core/message/index.js
@@ -0,0 +1,9 @@
+// AGENT NOTE:
+// SG 2.0 core message module barrel.
+// Purpose: provide stable imports for handleMessage-focused helpers.
+
+export * from "./messageInput.js";
+export * from "./messageAccess.js";
+export * from "./messageBehavior.js";
+export * from "./messageAiRequest.js";
+export * from "./messageGithubApproval.js";

385c65f9950cc198efd28e284b89c90f9d8e8482
korzh260609-beep <korzh260609@gmail.com>
2026-05-06 11:49:47 +0300

    refactor: add Telegram transport module barrel

---
+++ src/transport/telegram/index.js
@@ -0,0 +1,9 @@
+// AGENT NOTE:
+// SG 2.0 Telegram transport module barrel.
+// Purpose: provide stable imports for Telegram transport helpers.
+
+export * from "./telegramContext.js";
+export * from "./telegramReplies.js";
+export * from "./telegramMessageHandler.js";
+export * from "./telegramCallbackHandler.js";
+export * from "./telegramAdapter.js";

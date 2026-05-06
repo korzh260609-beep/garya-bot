472b8f8393314c2f036f647a3c10c848f865419b
korzh260609-beep <korzh260609@gmail.com>
2026-05-06 11:44:34 +0300

    refactor: add GitHub approval module barrel

---
+++ src/core/githubApproval/index.js
@@ -0,0 +1,8 @@
+// AGENT NOTE:
+// SG 2.0 GitHub approval core module barrel.
+// Purpose: provide stable imports for GitHub approval callback helpers.
+
+export * from "./githubApprovalCallbackParser.js";
+export * from "./githubApprovalAccess.js";
+export * from "./githubApprovalResultFormatter.js";
+export * from "./githubApprovalExecutor.js";

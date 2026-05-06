93d000f57890c6589e3113b1ff9cfd404963b54f
korzh260609-beep <korzh260609@gmail.com>
2026-05-06 14:06:56 +0300

    feat: export agent workspace runtime hook

--- src/agents/shared/workspace/index.js
+++ src/agents/shared/workspace/index.js
@@ -8,3 +8,4 @@ export * from "./AgentWorkspaceCleaner.js";
 export * from "./AgentWorkspaceCommandRunner.js";
 export * from "./AgentWorkspaceGithubGateway.js";
 export * from "./AgentWorkspaceReportWriter.js";
+export * from "./AgentWorkspaceRuntime.js";

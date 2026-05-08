488c932e1499feffb1dc8c6fdd90c791522cd435
korzh260609-beep <korzh260609@gmail.com>
2026-05-08 11:04:53 +0300

    agents: add repo commit watcher exports

---
+++ src/agents/repo-commit-watcher-agent/index.js
@@ -0,0 +1,5 @@
+// AGENT NOTE:
+// Public boundary for RepoCommitWatcherAgent.
+
+export { runRepoCommitWatcherAgent } from "./repoCommitWatcherAgent.js";
+export { findCommitsByIntent } from "./repoCommitSearch.js";

5243ed36aed7eacd130d0c53959d8bb3c76ea8db
korzh260609-beep <korzh260609@gmail.com>
2026-05-10 10:59:38 +0300

    diagnostics: export observation latest report check

--- src/diagnostics/index.js
+++ src/diagnostics/index.js
@@ -1,11 +1,9 @@
-// AGENT NOTE:
-// SG 2.0 Diagnostics Layer public boundary.
-// Purpose: expose diagnostics helpers without wiring them into transport or core directly.
-
 export * from "./diagnosticsIntent.js";
+export * from "./diagnosticsObservationBridge.js";
 export * from "./diagnosticsPlan.js";
 export * from "./diagnosticsReport.js";
 export * from "./diagnosticsRunner.js";
+export * from "./observationLatestReportCheck.js";
 export * from "./usersIdentityLinkingCheck.js";
 export * from "./usersIdentityLinkRequestsCheck.js";
 export * from "./usersIdentityRegistryCheck.js";

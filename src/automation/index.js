dc2f44b2cbc392a81f42da02172551ba1aa8d13a
korzh260609-beep <korzh260609@gmail.com>
2026-08-06 14:11:49 +0300

    Implement Block 9 Automation and Agents

---
+++ src/automation/index.js
@@ -0,0 +1,3 @@
+export { TASK_STATUSES, createAutomationTask, createDelegatedAgent, createAutomationEvent } from './contracts.js';
+export { createAutomationEngine } from './automationEngine.js';
+export { PREPARE_ONLY_KINDS, createPrepareOnlyCapability } from './prepareOnly.js';

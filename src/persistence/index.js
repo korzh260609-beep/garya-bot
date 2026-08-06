7c2109d604d626c065566370549af685679f1579
korzh260609-beep <korzh260609@gmail.com>
2026-08-06 15:43:18 +0300

    Block 12: compose PostgreSQL persistence

---
+++ src/persistence/index.js
@@ -0,0 +1,22 @@
+export { createPostgresDatabase } from './database.js';
+export { runMigrations } from './migrator.js';
+export { createPostgresRepositories } from './repositories.js';
+
+import { createPostgresDatabase } from './database.js';
+import { runMigrations } from './migrator.js';
+import { createPostgresRepositories } from './repositories.js';
+
+export function createPostgresPersistence(config = {}) {
+  const database = createPostgresDatabase(config);
+  const repositories = createPostgresRepositories(database);
+  return Object.freeze({
+    database,
+    repositories,
+    async start() {
+      await database.start();
+      await runMigrations(database);
+    },
+    close: () => database.close(),
+    health: () => database.health()
+  });
+}

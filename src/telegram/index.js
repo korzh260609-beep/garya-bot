23d93b88de0e407482d37192a6acc9b7e84167a3
korzh260609-beep <korzh260609@gmail.com>
2026-08-06 17:06:57 +0300

    Block 14: export Telegram production integration

---
+++ src/telegram/index.js
@@ -0,0 +1,5 @@
+export { createTelegramBotApiClient, TelegramApiError } from './telegramBotApiClient.js';
+export { createPostgresTelegramUpdateStore } from './postgresTelegramUpdateStore.js';
+export { evaluateTelegramInvocation, renderTelegramCommand } from './telegramInvocation.js';
+export { createInMemoryTelegramUpdateStore, createTelegramProductionIntegration } from './telegramProductionIntegration.js';
+export { createTelegramWebhookHttpHandler } from './telegramWebhookHttpHandler.js';

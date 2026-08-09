5690750504fe0701e951968c30c2392d42667734
korzh260609-beep <korzh260609@gmail.com>
2026-08-09 21:42:46 +0300

    Allow delivery attempt observability event

--- src/contracts/observability.js
+++ src/contracts/observability.js
@@ -13,6 +13,7 @@ export const OBSERVABILITY_EVENT_CLASSES = Object.freeze([
   'capability_completed',
   'capability_failed',
   'final_response_observed',
+  'delivery_attempt',
   'delivery_completed',
   'telegram_update_completed',
   'response_delivered',

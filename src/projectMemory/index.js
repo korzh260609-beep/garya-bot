a9738e17a65aa49812fcd10327e64ebdf1059941
korzh260609-beep <korzh260609@gmail.com>
2026-08-10 14:22:57 +0300

    Export PM3.1 project memory contract

---
+++ src/projectMemory/index.js
@@ -0,0 +1,15 @@
+export {
+  PROJECT_MEMORY3_VERSION,
+  PROJECT_MEMORY3_DOMAINS,
+  PROJECT_MEMORY3_FACT_TYPES,
+  PROJECT_MEMORY3_CONFIRMATION_STATES,
+  PROJECT_MEMORY3_RESERVED_AUTHORITY_KEYS,
+  SG21_PROJECT_MEMORY_NAMESPACES,
+  createProjectMemoryNamespace,
+  createProjectMemoryNamespaces,
+  parseProjectMemoryNamespace,
+  assertProjectMemoryNamespaceForProject,
+  createProjectFact,
+  assertProjectFactForProject,
+  selectProjectFactsForProject
+} from './projectFactContract.js';

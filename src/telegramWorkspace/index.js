c7d7b9ade039ceabd3d9dc2a38c83e01b470ec59
korzh260609-beep <korzh260609@gmail.com>
2026-08-12 11:37:55 +0300

    feat: export TWM1.1 workspace contract

---
+++ src/telegramWorkspace/index.js
@@ -0,0 +1,17 @@
+export {
+  TELEGRAM_WORKSPACE_PLATFORM,
+  TELEGRAM_WORKSPACE_TYPES,
+  TELEGRAM_WORKSPACE_LIFECYCLE,
+  TELEGRAM_WORKSPACE_ROLES,
+  TELEGRAM_WORKSPACE_CONFIG_NAMESPACES,
+  createTelegramWorkspace,
+  normalizeWorkspaceMigration,
+  migrateTelegramWorkspaceToSupergroup,
+  canTransitionTelegramWorkspace,
+  transitionTelegramWorkspace,
+  createTelegramWorkspaceScope,
+  assertTelegramWorkspaceScope,
+  assertTelegramWorkspace,
+  assertSameTelegramWorkspace,
+  telegramWorkspaceConfigNamespace
+} from './workspaceContract.js';

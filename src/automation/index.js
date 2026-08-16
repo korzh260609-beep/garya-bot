50eb46cd6d1d68e1a9301b64c90a5ed3eb0a80eb
korzh260609-beep <korzh260609@gmail.com>
2026-08-16 17:17:40 +0300

    feat(automation): export AW2.3 workflow executor API

--- src/automation/index.js
+++ src/automation/index.js
@@ -1,5 +1,7 @@
 export { TASK_STATUSES, createAutomationTask, createDelegatedAgent, createAutomationEvent } from './contracts.js';
 export { WORKFLOW_SCHEMA_VERSION, WORKFLOW_TRIGGER_TYPES, WORKFLOW_STEP_TYPES, assertSupportedWorkflowSchema, assertSupportedWorkflowStepType, createWorkflowStep, createWorkflowDefinition, adaptSelfNotificationTaskToWorkflow } from './workflowContract.js';
+export { WORKFLOW_EXECUTION_OUTCOMES, createWorkflowExecutor } from './workflowExecutor.js';
+export { createPostgresWorkflowExecutionStore } from './postgresWorkflowExecutionStore.js';
 export { createAutomationEngine } from './automationEngine.js';
 export { PREPARE_ONLY_KINDS, createPrepareOnlyCapability } from './prepareOnly.js';
 export { createPostgresTaskQueue } from './postgresTaskQueue.js';

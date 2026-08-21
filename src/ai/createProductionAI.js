a8feac5e6f939cb8ef7c058b4a34f4de83555633
korzh260609-beep <korzh260609@gmail.com>
2026-08-21 18:01:57 +0300

    Compose GH3 semantic routing into production AI

--- src/ai/createProductionAI.js
+++ src/ai/createProductionAI.js
@@ -1,9 +1,11 @@
 export * from './createProductionAICore.js';
 import { createProductionAI as createProductionAICore } from './createProductionAICore.js';
 import { publishProductionAIRouter } from './runtimeAIRouterBinding.js';
+import { createGitHubDevelopmentMeaningInterpreter } from '../githubDevelopment/githubDevelopmentMeaningInterpreter.js';
 
 export function createProductionAI(options = {}) {
   const productionAI = createProductionAICore(options);
+  const meaningInterpreter = createGitHubDevelopmentMeaningInterpreter({ baseInterpreter: productionAI.meaningInterpreter, aiRouter: productionAI.aiRouter });
   publishProductionAIRouter(productionAI.aiRouter);
-  return productionAI;
+  return Object.freeze({ ...productionAI, meaningInterpreter });
 }

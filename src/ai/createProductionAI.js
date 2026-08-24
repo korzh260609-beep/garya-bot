3a3923d1a998316bac20d3c0ba9d9720cce08f98
korzh260609-beep <korzh260609@gmail.com>
2026-08-24 09:20:41 +0300

    fix: remove stale GitHub meaning interpreter wiring

--- src/ai/createProductionAI.js
+++ src/ai/createProductionAI.js
@@ -1,11 +1,9 @@
 export * from './createProductionAICore.js';
 import { createProductionAI as createProductionAICore } from './createProductionAICore.js';
 import { publishProductionAIRouter } from './runtimeAIRouterBinding.js';
-import { createGitHubDevelopmentMeaningInterpreter } from '../githubDevelopment/githubDevelopmentMeaningInterpreter.js';
 
 export function createProductionAI(options = {}) {
   const productionAI = createProductionAICore(options);
-  const meaningInterpreter = createGitHubDevelopmentMeaningInterpreter({ baseInterpreter: productionAI.meaningInterpreter, aiRouter: productionAI.aiRouter });
   publishProductionAIRouter(productionAI.aiRouter);
-  return Object.freeze({ ...productionAI, meaningInterpreter });
+  return productionAI;
 }
